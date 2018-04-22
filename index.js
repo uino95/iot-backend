60253fdff9f8bad019f2506e6caa7e1a6554d9c2
Matteo Sinico <sinicomatteo@gmail.com>
2018-04-22 16:13:36 +0200

    Modified heroku app to be a subscriber to all the topic and save all the messages to mongodb from where retrieve them

--- index.js
+++ index.js
@@ -10,50 +10,35 @@ var mqtt = require('mqtt');
 
 
 ////////////////// SETTINGS /////////////////
-
-var broker_url = "mongodb://mbare:ciao@ds239177.mlab.com:39177/broker_db";
+//MongoDb url on mlab
+var db_url = "mongodb://mbare:ciao@ds239177.mlab.com:39177/broker_db";
+//CloudMQTT broker url on heroku
 var mqtt_url = 'mqtt://bqmqptlw:bUouMU6bIdPx@m14.cloudmqtt.com:10671';
 var brokerDbo;
 
-// var ascoltatore = {
-//   //using ascoltatore
-//   type: 'mongo',    
-//   url: "mongodb://mbare:ciao@ds253959.mlab.com:53959/mqtt",
-//   pubsubCollection: 'ascoltatori',
-//   mongo: {}
-// };
-
-// var moscaSettings = {
-//   port: 1883,
-//   backend: ascoltatore,
-//   persistence: {
-//     factory: mosca.persistence.Mongo,
-//     url: "mongodb://mbare:ciao@ds253959.mlab.com:53959/mqtt"
-//   }
-// };
-
-///////////// Init Database /////////////////
+///////////// Init mqtt client ///////////////
 
 var client = mqtt.connect(mqtt_url)
 
-MongoClient.connect(broker_url, function(err, db) {
+client.on('connect', function () {
+  //here we subscribe to all our topic
+  client.subscribe('new/prova')
+  console.log("subscribed successfully")
+})
+
+///////////// Init Database /////////////////
+
+MongoClient.connect(db_url, function(err, db) {
   if(err) throw err;
   brokerDbo = db.db("broker_db");
   console.log("connected to broker db");
 })
 
-
-// var server = new mosca.Server(moscaSettings);
-// server.on('ready', setup);
-
-
-////////////////// APP //////////////////
-
+///////////// Handle App ///////////////////
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get("/messages",  function(req, res){
 	let query = brokerDbo.collection("messages").find({}).toArray(function(err, result) {
@@ -62,40 +47,21 @@ app.get("/messages",  function(req, res){
   	});
 });
 
-///////////// Manage Received Messages //////////////
+///////////// Handle Received Messages //////////////
 
 client.on('message', function (topic, message) {
   // message is Buffer
+  var msg = {
+    topic: topic,
+    message: message
+  }
   console.log("Got new message! Topic: " + topic + "; Message: " + message)
-  brokerDbo.collection("messages").insertOne(message, function(err, res) {
+  brokerDbo.collection("messages").insertOne(msg, function(err, res) {
      if (err) throw err;
      console.log("message saved");
   });
   // client.end()
 })
-
-// server.on('clientConnected', function(client) {
-//   console.log('client connected', client.id);   
-// });
-
-// fired when a message is received
-// server.on('published', function(packet, client) {
-//   var message;
-//   message = {
-//     topic: packet.topic,
-//     payload: packet.payload.toString()
-//   };
-//   console.log('Published', message);  
-//   brokerDbo.collection("messages").insertOne(message, function(err, res) {
-//     if (err) throw err;
-//     console.log("message saved");
-//   });
-// });
-
-// fired when the mqtt server is ready
-// function setup() {
-//   console.log('Mosca server is up and running')
-// }
 
 //////// Instatiate the app ///////////
 
